c2bf061460247fd80ab29de247f901c7859f5c27
noknuan <noknuan@hotmail.com>
2018-03-13 18:45:56 +1300

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,13 +25,13 @@
                     {!! Form::open(['url'=>'/login']) !!}
                     <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
                         {!! Form::label("username","Username") !!}
-                        {!! Form::text("username",null,["class"=>"form-control","placeholder"=>"Username"]) !!}
+                        {!! Form::text("username",null,["class"=>"form-control","placeholder"=>"Enter your username"]) !!}
                         {!! $errors->first('username','<span class="help-block">:message</span>') !!}
                     </div>
 
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                         {!! Form::label("password","Password") !!}
-                        {!! Form::password("password",["class"=>"form-control","placeholder"=>"Password"]) !!}
+                        {!! Form::password("password",["class"=>"form-control","placeholder"=>"Enter your password"]) !!}
                         {!! $errors->first('password','<span class="help-block">:message</span>') !!}
                     </div>
                     <input type="hidden" name="active" value="1"/>
