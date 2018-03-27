1cfa08d17b9cf2a959f7f636e69475b5da9686fe
noknuan <noknuan@hotmail.com>
2018-03-27 22:34:01 +1300

    timeout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,12 +15,12 @@
     <div class="row-fluid" style="margin-top: 5px">
         <div class="col-md-6 col-md-offset-3" style="padding-top: 100px;padding-bottom: 10px ">
             <div class="row" >
-                <div class="col-sm-5 hidden-xs" style="text-align: center ; background-color: #2aabd2; padding-top: 56px;padding-bottom: 71px; border: double">
+                <div class="col-sm-5 hidden-xs" style="text-align: center ; background-color: #2aabd2; padding-top: 56px;padding-bottom: 70px; border: double #a91715">
                     <img src="{{asset("images/logo.png")}}" width="200" height="100"/>
                     <h4>Point Of Sale System</h4>
                 </div>
 
-                <div class="col-sm-7" style="background: #ffea23; padding-top: 50px ;padding-bottom: 19px;border: double">
+                <div class="col-sm-7" style="background: #ffea23; padding-top: 50px ;padding-bottom: 19px;border: double #a91715">
                     {!! Form::open(['url'=>'/login']) !!}
                     <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
                         {!! Form::label("username","Username") !!}
