1fb348de93fc448d6070f5c279371f6e1cc99404
noknuan <noknuan@hotmail.com>
2018-04-06 12:08:25 +1200

    pdf

--- resources/views/cashier/printpdf.blade.php
+++ resources/views/cashier/printpdf.blade.php
@@ -9,30 +9,10 @@
             font-family: 'THSarabunNew';
             font-style: normal;
             font-weight: normal;
-            src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
+            src: url("http://eclecticgeek.com/dompdf/fonts/Elegance.ttf") format("truetype");
 
         }
-        @font-face {
-            font-family: 'THSarabunNew';
-            font-style: normal;
-            font-weight: bold;
-            src: url("{{ public_path('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
 
-        }
-        @font-face {
-            font-family: 'THSarabunNew';
-            font-style: italic;
-            font-weight: normal;
-            src: url("{{ public_path('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
-
-        }
-        @font-face {
-            font-family: 'THSarabunNew';
-            font-style: italic;
-            font-weight: bold;
-            src: url("{{ public_path('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
-
-        }
         body {
             font-family: "THSarabunNew";
         }
