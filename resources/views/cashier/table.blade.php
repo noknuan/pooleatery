831c44e6f04c07b38d47628e9c040660f1c78a7a
noknuan <noknuan@hotmail.com>
2018-04-11 22:34:20 +1200

    table status

--- resources/views/cashier/table.blade.php
+++ resources/views/cashier/table.blade.php
@@ -2,10 +2,10 @@
     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
     </button>
     <h4 class="modal-title" style="padding-bottom: 4px">Select Table</h4>
-    Status : 
+    Status ::
     <img src="{{asset('images/free.PNG')}}" height ="20px" width="20px"/> Free
     <img src="{{asset('images/busy.PNG')}}" height="20px" width="20px"/> Busy
-    <img src="{{asset('images/invoice.PNG')}}" height="20px" width="20px"/> Print Invoice
+    <img src="{{asset('images/invoice.PNG')}}" height="20px" width="20px"/> Invoice
 </div>
 <div class="modal-body">
     <div class="row">
