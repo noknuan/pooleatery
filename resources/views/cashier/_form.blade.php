c2d81bc7252ea77a96a83864108519873e99c8e4
noknuan <noknuan@hotmail.com>
2018-05-04 14:34:57 +1200

    modify css

--- resources/views/cashier/_form.blade.php
+++ resources/views/cashier/_form.blade.php
@@ -1,17 +1,17 @@
 <div class="modal-body">
         <div class="form-group required" id="form-description-error">
             {!! Form::label("description","Description",["class"=>"control-label"]) !!}
-            {!! Form::text("description",null,["class"=>"form-control required","id"=>"focus","autocomplete"=>"off"]) !!}
+            {!! Form::text("description",null,["class"=>"form-control required","id"=>"focus","autocomplete"=>"off","placeholder"=>"Enter gdescription"]) !!}
             <span id="description-error" class="help-block"></span>
         </div>
         <div class="form-group required" id="form-quantity-error">
             {!! Form::label("quantity","Quantity",["class"=>"control-label"]) !!}
-            {!! Form::text("quantity",null,["class"=>"form-control required","autocomplete"=>"off"]) !!}
+            {!! Form::text("quantity",null,["class"=>"form-control required","autocomplete"=>"off","placeholder"=>"Enter quantity"]) !!}
             <span id="quantity-error" class="help-block"></span>
         </div>
         <div class="form-group required" id="form-price-error">
             {!! Form::label("price","Price",["class"=>"control-label"]) !!}
-            {!! Form::text("price",null,["class"=>"form-control required","autocomplete"=>"off"]) !!}
+            {!! Form::text("price",null,["class"=>"form-control required","autocomplete"=>"off","placeholder"=>"Enter price"]) !!}
             <span id="price-error" class="help-block"></span>
         </div>
 </div>
