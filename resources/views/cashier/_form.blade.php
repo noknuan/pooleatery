73c515d1f4070a43d6a00a11ef88e5f098ef407d
noknuan <noknuan@hotmail.com>
2018-05-05 09:48:22 +1200

    modify css

--- resources/views/cashier/_form.blade.php
+++ resources/views/cashier/_form.blade.php
@@ -16,8 +16,8 @@
         </div>
 </div>
 <div class="modal-footer">
-    {!! Form::button("<i class='glyphicon glyphicon-remove'></i> Close",["class"=>"btn
-    btn-danger","data-dismiss"=>"modal"])!!}
     {!! Form::button("<i class='glyphicon glyphicon-floppy-disk'></i> Save",["type" => "submit","class"=>"btn
     btn-primary","id"=>"btn_save"])!!}
+    {!! Form::button("<i class='glyphicon glyphicon-remove'></i> Close",["class"=>"btn
+    btn-danger","data-dismiss"=>"modal"])!!}
 </div>
