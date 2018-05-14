9fc48c7958fa31c842a40f1ca2f822b532705fe8
noknuan <noknuan@hotmail.com>
2018-05-14 13:21:02 +1200

    product export pdf

--- resources/views/product/print.blade.php
+++ resources/views/product/print.blade.php
@@ -4,23 +4,25 @@
         page-break-after: always;
     }
 </style>
-<h1 class="page-header">Product List </h1>
+<?php $i = 1;?>
+<h1>Product List </h1>
     <table style="width:100%">
         <thead>
-        <tr style="font-size:14px">
+        <tr style="font-size:14px; ">
             <th width="20%" style="text-align: center" bgcolor="#a9a9a9"> Code</th>
             <th width="50%" style="text-align: center" bgcolor="#a9a9a9"> Name</th>
             <th width="30%" style="text-align: center" bgcolor="#a9a9a9">Unit Price (Bath) </th>
         </tr>
         </thead>
         <tbody style="font-size: 12px">
+
         @foreach($categories as $key=>$category)
             <tr>
                 <th colspan="3">{{$category->name}}</th>
             </tr>
             @foreach($category->products as $product)
                 <tr>
-                    <td align="center">{{$product->id}}</td>
+                    <td align="center">{{$i++}}</td>
                     <td>{{$product->name}}</td>
                     <td align="right">{{$product->unitprice}}</td>
                 </tr>
@@ -28,7 +30,9 @@
             <tr>
                 <td colspan="3"><hr></td>
             </tr>
-
         @endforeach
         </tbody>
     </table>
+<h4>Total: {{$i-1}} records</h4>
+
+
